e150649838fc18c83f3e4a9b7669ceec4cf684e0
sankaleshPal <sankalesh_h@paltribe.in>
2026-05-10 15:04:25 +0530

    fix: first commit

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from '@/queries/queryClient';
 import { RootNavigator } from '@/navigation/RootNavigator';
-
+//sankalesh
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
